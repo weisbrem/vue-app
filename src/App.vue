f94d69de598f2ade67ad8d86f722224150511d57
weisbrem <zhernovkov_konst@mail.ru>
2024-05-11 00:49:46 +0200

    feat: add card and card list components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import MainHeader from '@/components/MainHeader.vue'
+import CardList from '@/components/CardList.vue'
 </script>
 
 <template>
-  <div class="w-4/5 h-screen m-auto mt-14 bg-white rounded-xl shadow-xl">
+  <div class="w-4/5 m-auto mt-14 bg-white rounded-xl shadow-xl">
     <MainHeader />
+
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+      <CardList />
+    </div>
   </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import CardElement from './CardElement.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <CardElement
+      image-url="/sneakers/sneakers-1.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="12999"
+      :is-favorite="false"
+      :is-added="true"
+      :on-favorite-click="() => console.log('favorite test click')"
+      :on-add-click="() => console.log('add test click')"
+    />
+  </div>
+</template>

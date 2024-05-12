2dc2252ffa6c96c171f203430487ba248d993dfa
weisbrem <zhernovkov_konst@mail.ru>
2024-05-12 19:45:30 +0200

    feat: init router

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
+import { router } from './router';
 import App from './App.vue';
 
-createApp(App).use(autoAnimatePlugin).mount('#app');
+const app = createApp(App);
+app.use(autoAnimatePlugin);
+app.use(router);
+app.mount('#app');
